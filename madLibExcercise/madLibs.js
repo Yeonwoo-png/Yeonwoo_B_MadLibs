566b67781b9d629ee8f2e49b985a7b7b31123935
Yeonwoo <yeonwoobae2003@gmail.com>
2025-03-28 01:24:27 -0700

    assigned values to different words

--- madLibExcercise/madLibs.js
+++ madLibExcercise/madLibs.js
@@ -6,6 +6,16 @@ Numbers (e.g., const mathResult = 2 + 2;).
 A boolean expression (e.g., const booleanValue = timeOfDay.includes("afternoon");).
 An object with properties (e.g., const person = { name: "Mike", age: 25 };).
 */
+
+const timeOfDay = "stormy night";
+const numbers = 10 - 7;
+const booleanValue = timeOfDay.includes("night");
+const ghost = {
+    name: Spooky,
+    eyes: glowing,
+    body: levitating
+};
+
 // Step 1: Paste in one of the story templates above
 const story = `
 // todo
